3156435885d0a9f327d22b5eb9690a6fdec39260
Nick Chen <nick.chen053100@gmail.com>
2018-02-20 18:27:44 -0600

    implemented Ping-Pong Class

---
+++ IdeaProjects/CAHSecretary/src/main/java/cah/secretary/bot/Bot.java
@@ -0,0 +1,16 @@
+package cah.secretary.bot;
+
+import net.dv8tion.jda.core.AccountType;
+import net.dv8tion.jda.core.JDA;
+import net.dv8tion.jda.core.JDABuilder;
+
+
+/**
+ * @author ZeXu
+ */
+public class Bot {
+    public static void main(String[] args) throws Exception {
+        JDA api = new JDABuilder(AccountType.BOT).setToken("NDE1NjA2OTcyMjA2MDIyNjU5.DW5Hkg.kIof7R10-fKa-XqEfBLflyb262A").buildAsync();
+        api.addEventListener(new PingPong());
+    }
+}
